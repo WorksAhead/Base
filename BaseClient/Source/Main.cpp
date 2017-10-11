d1cdd3b65f8d055115f61bfe4b58b8c696d05844
amanjiang <amanjiang@gmail.com>
2017-10-11 17:25:23 +0800

    修复内容相关版本列表中无法正常显示父版本超链接的问题。

--- BaseClient/Source/Main.cpp
+++ BaseClient/Source/Main.cpp
@@ -37,7 +37,7 @@
 #include <windows.h>
 #endif
 
-#define BASE_CURRENT_VERSION "1.0.0.52"
+#define BASE_CURRENT_VERSION "1.0.0.53"
 
 namespace fs = boost::filesystem;
 
--- BaseClient/Source/PageContentContentWidget.cpp
+++ BaseClient/Source/PageContentContentWidget.cpp
@@ -470,7 +470,7 @@ void PageContentContentWidget::refreshRelatedVersions(int count)
 			if (context_->session->getContentInfo(ci.parentId, ci2) == Rpc::ec_success)
 			{
 				if (ci2.state == "Normal" || ci2.state == "Hidden") {
-					items.push_back({ci2.id, ci2.title});
+					items.push_back({ci2.id, ci2.title, ci2.state});
 				}
 			}
 
